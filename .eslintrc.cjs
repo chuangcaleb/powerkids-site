78e40dcc6c30e7ccbeac2423e38f6c8269e24008
chuangcaleb <balechuang@gmail.com>
2023-07-19 17:38:36 +0800

    feat: initial setup
    astro, shadcn (react/tailwind/radix ui), prettier/eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "react/react-in-jsx-scope": ["off"],
+      },
+    },
+    {
+      files: [".jsx", ".tsx"],
+      extends: ["plugin:react/recommended"],
+      plugins: ["react"],
+      rules: {},
+    },
+  ],
+  globals: {
+    astroHTML: true,
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tabWidth: 2,
+  singleQuote: false,
+  astroAllowShorthand: false,
+};
