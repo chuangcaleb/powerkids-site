d641af9f6bb43d4dc6e786b39065b3b360f31cab
chuangcaleb <balechuang@gmail.com>
2023-07-28 00:51:07 +0800

    fix: use fileURLToPath in vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import compressor from "astro-compressor";
 import { defineConfig } from "astro/config";
-import path from "path";
+import { fileURLToPath, URL } from "url";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,7 +22,7 @@ export default defineConfig({
     },
     resolve: {
       alias: {
-        "@": path.resolve(import.meta.url, "./src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
   },
